f4630a3f5866f8c856230de08d13db6afd133752
clingfei <1599101385@qq.com>
2022-01-28 21:16:11 +0800

    finish basic shell

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 #define LSH_RL_BUFSIZE 1024
 #define LSH_TOK_BUFSIZE 64
 
+inline int lsh_exit(char **args) {
+    return 0;
+}
+
+inline int lsh_builtin_num() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
 
 char* lsh_read_line() {
     /*
@@ -98,7 +103,70 @@ char** lsh_split_line(char *lines) {
 }
 
 int lsh_execute(char **args) {
+    int i = 0;
 
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < lsh_builtin_num(); ++i) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
+
+int lsh_cd(char **args) {
+    if (args == NULL) {
+        fprintf(stderr, "expected argument to \"cd\"\n");
+    }
+    else {
+        if (chdir(args[1]) != 0) {
+            perror("cd error\n");
+        }
+    }
+    return 1;
+}
+
+int lsh_help(char **args) {
+    int i;
+    printf("Type program names and arguments\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_builtin_num(); ++i) {
+        printf("%s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for other program information, like \"man shell\".\n");
+    return 1;
+}
+
+int lsh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("execvp error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0) {
+        perror("fork error\n");
+    }
+    else {
+        wpid = waitpid(pid, &status, WUNTRACED);
+        if (WIFSIGNALED(status)) {
+            printf("Child exited via signal %d\n", WTERMSIG(status));
+        }
+        else if (WIFEXITED(status)) {
+            printf("Child exited via signal %d\n", WEXITSTATUS(status));
+        }
+    }
+    return 1;
 }
 
 void lsh_loop() {
@@ -110,6 +178,7 @@ void lsh_loop() {
         printf("> $");
         lines = lsh_read_line();
         if (lines == NULL) continue;
+        printf("%s\n", lines);
         args = lsh_split_line(lines);
         if (args == NULL) continue;
         status = lsh_execute(args);
--- main.h
+++ main.h
@@ -1,8 +1,45 @@
 //
 // Created by 15991 on 2022/1/27.
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #ifndef TINYSHELL_MAIN_H
 #define TINYSHELL_MAIN_H
+
+char *builtin_str[] = {
+        "cd",
+        "help",
+        "exit"
+};
+
+int lsh_builtin_num();
+
+int lsh_exit(char **args);
+
+int lsh_cd(char **args);
+
+void lsh_loop();
+
+int lsh_help(char **args);
+
+char* lsh_read_line();
+
+char** lsh_split_line(char *lines);
+
+int lsh_execute(char **args);
+
+int lsh_launch(char **args);
+
+int (*builtin_func[]) (char **) = {
+        &lsh_cd,
+        &lsh_help,
+        &lsh_exit
+};
 
 #endif //TINYSHELL_MAIN_H
